ec921e08addaec82c53b49cec149e9392e000637
Ericsson Colborn <ericssoncolborn@Ericssons-Air.cable.rcn.com>
2022-08-07 19:23:01 +0000

    started Main class

---
+++ zackage/Main.java
@@ -0,0 +1,103 @@
+package zackage;
+import java.util.*;
+import java.io.*;
+
+/* ***************************************************************************************
+                    MAIN.JS - DRIVER PROGRAM FOR (INSERT PROJECT NAME)
+****************************************************************************************** */
+public class Main {
+    
+/*
+    loadPosts()
+    - Loads posts from /post folder (stored in .bin files) into memory
+    * @params: none
+    * @return: an ArrayList<Post> of all Posts that were read into memory
+*/
+
+    public static ArrayList<Post> loadPosts() throws FileNotFoundException, IOException {
+
+        // initialize an ArrayList to store all Posts
+        ArrayList<Post> readPosts = new ArrayList<Post>();
+        // path to the folder containing .bin files for all Posts
+        String pathToPostsFolder = "/Users/ericssoncolborn/Documents/NYU/Summer_22/OOP-final-project/zackage/posts";
+
+        File postsFolder = new File(pathToPostsFolder);
+        File[] listOfFiles = postsFolder.listFiles();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+
+            // get name of file ( should be {PostID}.bin )
+            String filename = listOfFiles[i].getName();
+
+            // read that Post into an object using Post.readPost()
+            Post p = Post.readPost(filename);
+
+            // add the newly read Post to the ArrayList
+            readPosts.add(p);
+        }
+
+        // return ArrayList of Posts
+        return readPosts;
+    }
+
+
+// ******* OPTIONAL WAY TO IMPLEMENT POST SAVE - alternative is to save on Post creation in Post constructor ********
+/*
+    savePosts()
+    - Saves posts in memory to the /posts folder as.bin files
+    - Run on (intentional) program exit
+    * @params: none
+    * @return: an ArrayList<Post> of all Posts that were read into memory
+*/
+    public static boolean savePosts() throws FileNotFoundException, IOException {
+
+        return true;
+    }
+
+
+
+/* *********************************************
+        METHODS FOR DISPLAYING UI's
+
+    * essentially text wrappers for User
+    and Post methods that implement
+    application logic
+
+    -> all except menu() call a method in the
+    Post or User class with a similar name
+************************************************ */
+
+
+    public static void menu() {
+
+    }
+
+    public static void createPost() {
+
+    }
+
+    public static void deletePost() {
+
+    }
+
+
+
+
+
+
+
+/* *********************************************** 
+                MAIN() FUNCTION
+************************************************** */
+    public static void main(String[] args) {
+        
+        // Post p = new Post();
+
+        try {
+            Main.loadPosts();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+}
